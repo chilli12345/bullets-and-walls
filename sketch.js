c63372cd382041483b09c5703283232d26600273
chilli12345 <73940796+chilli12345@users.noreply.github.com>
2020-11-17 10:33:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
 var bullet, wall;
 var weight, speed, thickness;
+var bulletImage;
+
+function preload(){
+  bulletImage=loadImage("download.jpg");
+}
 
 function setup () {
   createCanvas(1600,400);
@@ -10,6 +15,8 @@ function setup () {
   speed=random(223,321);
 
   bullet=createSprite(50,200,40,20);
+  bullet.addImage(bulletImage);
+  bullet.scale=0.5;
   bullet.shapeColor="white"
   wall=createSprite(1200,200,thickness,height/2);
   wall.shapeColor="grey";
@@ -19,12 +26,7 @@ function setup () {
 function draw(){
   background(0);
   bullet.velocityX=speed;
-  collide();
-  drawSprites();
-}
-
-function collide(){
-  if(wall.x-bullet.x < (bullet.width+wall.width)/2){
+  if(hasCollided(bullet,wall)){
     bullet.velocityX=0;
     var damage=0.5*weight*speed*speed/(thickness * thickness * thickness);
     if(damage<10){
@@ -32,12 +34,28 @@ function collide(){
       textSize(30);
       fill("green");
       text("Wall is effective",600,200);
+      text("Weight= "+Math.round(weight),600,150);
+      text("thickness= "+Math.round(thickness),600,100);
+      text("speed= "+Math.round(speed),600,60);
     }
-    else if(damage>10){
+    if(damage>10){
       wall.shapeColor="red";
       textSize(30);
       fill("red");
       text("Wall is not effective",600,200);
+      text("Weight= "+Math.round(weight),600,150);
+      text("thickness= "+Math.round(thickness),600,100);
+      text("speed= "+Math.round(speed),600,60);
     }
   }
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x +lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false
 }
